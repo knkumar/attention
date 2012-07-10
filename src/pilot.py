35f2cd2e8493b24f0c02c2bbe29a0f99df7c5f35
Suyog Chandramouli <suyoghc@gmail.com>
2012-07-10 01:37:22 -0400

    user input, response time

--- src/pilot.py
+++ src/pilot.py
@@ -56,13 +56,30 @@ def drawCircles(images, video, mypc, posRed):
     for i,item in enumerate(pos):
         if i == posRed:
             video.showProportional(Image(images["red"][gabor_switch[i]]) , item[0], item[1])
+            orientation=gabor_switch[i]
         else:
             video.showProportional(Image(images["green"][gabor_switch[i]]) , item[0], item[1])
 
     video.updateScreen()
-    mypc.delay(1000) 
+    
+    #mypc.delay(1000) 
     # make sure we've finished displaying
-    mypc.wait() 
+    #mypc.wait() 
+    
+    #0-h, 1-v
+    return orientation
+
+
+def userInput(stim,bc,patchOrientation):
+    ts, b, rt = stim.present(clk=mypc,duration=0,bc=bc)
+    response_time = rt[0]-ts[0]
+    if b==Key(patchOrientation):
+        result = "Correct!"
+    else:
+        result = "Incorrect!"
+    flashStimulus(Text(result), clk=mypc)
+    
+    return result,response_time
 
 def experiment(num_runs):
     # create an experiment object
@@ -73,8 +90,16 @@ def experiment(num_runs):
     mypc = PresentationClock()
     images = createOverlay(("red","green"))
     sequence = random.sample(range(12),num_runs)
+    
+    stim = Text("", color="black")
+    keyboard = KeyTrack("keyboard")
+    bc = ButtonChooser(Key("H"), Key("V"))
+    #orientation='H'
     for pos in sequence:
-        drawCircles(images,video,mypc, pos)
+        patchOrientation=drawCircles(images,video,mypc, pos)
+        result,response_time=userInput(stim,bc,patchOrientation)
+        
+        
     
 if __name__ == "__main__":
     experiment(10)
