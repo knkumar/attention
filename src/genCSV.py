0f70ce8f39e231cc42d18de4e39f5985236a6b22
Kiran <krankumar@gmail.com>
2012-08-17 12:04:05 -0400

    spanking new csv generation

--- src/genCSV.py
+++ src/genCSV.py
@@ -2,16 +2,53 @@
 from optparse import OptionParser
 import os 
 
-def gen_csv(fname):
-    session = open("data/"+fname+"/session.log", "rb")
-    csv = open("results/"+fname+".csv", "wb")
+def match():
+    f = open("results/mapping.txt","rb")
+    mapping = {}
+    for line in f.readlines():
+        k,v = line.split(",")
+        mapping[k.strip()] = v.strip()
+    return mapping
+
+def gen_csv(fname, mapping):
+    name = fname[:-4]
+    ID = mapping[name]
+    day = fname[-4:]
+    session = open("data/gabor/"+fname+"/session.log", "rb")
+    csv = open("results/gabor/"+ID+day+".csv", "wb")
     # the format for the file
-    header = "TrialNum,Response,ResponseTime,Target,Item1,Item2,Item3,NumDist\n"
+    header = "ID, day,TrialNum,Response,RT,T,TOr,TPos,D1,D1Or,D1Pos,D2,D2Or,D2Pos,NumDist,Presence\n"
+    positions = {"square":4, "red":5, "size":6}
+
+    def drill(cols, position, out):
+        item, other = cols[position].split("=")
+        orient,pos = other.split("|")
+        out.append(item)
+        out.append(orient)
+        out.append(pos)
+
     csv.write(header)
     for line in session.readlines():
+        dists = [4,5,6]
         main = line.split('\t')[2]
         if ',' in main:
-            csv.write(main[:]+"\n")
+            cols = main.split(",")
+            out = [ID,day]
+            out.extend(cols[:3])
+            target = positions[cols[3]]
+            dists.remove(target)
+            if cols[target]:
+                drill(cols, target, out)
+            else:
+                out.extend["","",""])
+            if cols[dists[0]]:
+                drill(cols, dists[0], out)
+            if cols[dists[1]]:
+                drill(cols, dists[1], out)
+            out.extend(["","",""]* (2-int(cols[7])) )
+            out.extend(cols[7:])
+            print out
+            csv.write(",".join(out))
     session.close()
     csv.close()
 
@@ -24,15 +61,16 @@ if __name__ == "__main__":
 
     (options, args) = parser.parse_args()
 
+    mapping = match()
 
     if not (options.file or options.all):
         print "Usage is getStats.py <-f|--file file> <-a|-all>"
     
     if options.file:
-        gen_csv(options.file)
+        gen_csv(options.file, mapping)
 
     if options.all:
-        names = os.popen("ls data")
+        names = os.popen("ls data/gabor")
         for name in names.readlines():
-            gen_csv(name.strip())
+            gen_csv(name.strip(),mapping)
             print "done "+name.strip()
